3e70b1de0fccb0b03854a8b251489d9779e617f8
vsoupdotvx <vsoupdotvx@github.com>
2025-01-15 02:07:09 +0000

    Fix and enable random firerates (not random yet)

--- src/asm/firerates.s
+++ src/asm/firerates.s
@@ -7,17 +7,17 @@
 plant_get_firerate:
     movl Plant.thePlantType(%rbx), %edx
     cmpl $1160, %edx #MAX_PLANT
-    jmp  plant_get_firerate.locA
+    ja   plant_get_firerate.locA
         call     plant_type_flatten
         
         leaq     plant_firerate_table(%rip), %rdx
         movzbq   (%rdx,%rax), %rdx
         shlb     $1,           %dl
         cvtsi2ss %edx,       %xmm6
-        mulss    const1over508(%rip), %xmm6
-        addss    const0.5(%rip), %xmm6
-        jnc      plant_get_firerate.locB
-            mulss const0.25(%rip), %xmm6
+        mulss    const1over254(%rip), %xmm6
+        addss    const1.0(%rip), %xmm6
+        jc       plant_get_firerate.locB
+            mulss const0.5(%rip), %xmm6
     jmp plant_get_firerate.locB
     plant_get_firerate.locA:
         movss const1.0(%rip), %xmm6
@@ -30,9 +30,7 @@ const1.0:
     .float 1.0
 const0.5:
     .float 0.5
-const0.25:
-    .float 0.25
-const1over508:
-    .float 0.00196850393701
+const1over254:
+    .float 0.00393700787402
 plant_firerate_table:
-    .space 33 + 12 + 25 + 161, 0x80
+    .space 33 + 12 + 25 + 161, 0x00
