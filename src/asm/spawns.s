4ee6f2c88f9347c31c281399187232f112694d89
vsoupdotvx <vsoupdotvx@github.com>
2025-02-08 22:13:24 +0000

    Implement random zombie weights

--- src/asm/spawns.s
+++ src/asm/spawns.s
@@ -7,9 +7,11 @@
 "InitZombieList::AdvantureZombieTypeSpawn(theLevelNumber: i32)+0x82":
 	jmp init_zombie_list
 "ENDInitZombieList::AdvantureZombieTypeSpawn(theLevelNumber: i32)+0x82":
-nop #the assembled object file has no distinction between if the last jump is
-#trying to branch inside or outside the patch and choses inside if its a jmp
-#instruction, which is wrong. this nop corrects that
+
+"InitZombieList::PickZombie() -> ZombieType":
+	jmp pick_zombie
+"ENDInitZombieList::PickZombie() -> ZombieType":
+
 init_zombie_list:
 	movq %rcx, %rbx
 	
@@ -40,6 +42,47 @@ init_zombie_list:
 	
 	addq $0x20, %rsp
 	popq %rbx
+	ret
+
+pick_zombie:
+	pushq %rbp
+	pushq %rbx
+	movq  %rsp, %rbp
+	
+	leaq zombie_spawn_bitfield(%rip), %r8
+	xorl %edx, %edx
+	movl $127, %eax
+	pick_zombie.loopA:
+		movq %rax,         %r9
+		movl %eax,        %ecx
+		andq $0x3F,        %r9
+		shrl $6,          %ecx
+		btq  %r9, (%r8,%rcx,8)
+		jnc pick_zombie.locA
+			addl  16(%r8,%rax,4), %edx
+			pushq %rax
+		pick_zombie.locA:
+		subl $1, %eax
+	jnc pick_zombie.loopA
+	
+	movq %rsp,  %rbx
+	xorl %ecx,  %ecx
+	andq $-16,  %rsp
+	subq $0x20, %rsp
+	call "UnityEngine::Random::RandomRangeInt(minInclusive: i32, maxExclusive: i32) -> i32"
+	movq %rbx,  %rsp
+	
+	leaq zombie_weights(%rip), %rdx
+	pick_zombie.loopB:
+		popq %rcx
+		subl (%rdx,%rcx,4), %eax
+	jnc pick_zombie.loopB
+	
+	call zombie_type_widen
+	
+	movq %rbp, %rsp
+	popq %rbx
+	popq %rbp
 	ret
 
 .section .data
